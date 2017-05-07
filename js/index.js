61faf7308a0ca7d2a6b67c147bf6f1ccbf3ebad1
Christopher Wong <cwong@christopherwong.co>
2017-05-07 17:42:28 -0400

    new js

--- js/index.js
+++ js/index.js
@@ -3,12 +3,13 @@ $("#main").fadeIn(1000);
 
 $(document).ready(function (){
   $("#show_email").click(function() {
-    $(".email").fadeToggle()
+    $(this).addClass("active");
+    $(".email").fadeToggle();
   });
 } );
 
 document.onkeypress = function(e) {
-  if (e.keyCode === 112 || window.event) {
-    window.location="projects.html"
+  if (e.keyCode === 112) {
+    window.location="projects.html";
   }
 }
